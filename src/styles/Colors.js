9b4de77a48d584f86a6ce25d899ca622ef2084a3
pscroce <pscroce@gmail.com>
2018-07-29 20:10:59 -0400

    style: add color variables

--- src/styles/Colors.js
+++ src/styles/Colors.js
@@ -1,9 +1,8 @@
 export default {
-  primary: "lightblue",
-  secondary: "maroon",
-  lightGray: "#eee",
-  midGray: "#3d3d3d",
-  darkGray: "#555",
+  primary: "#96d2b7", // Statue of Liberty
+  secondary: "#0039A6", // MTA
+  lightGray: "#A7A9AC", // L
+  darkGray: "#808183", // S
 
   textColor: "#3d3d3d",
   backgroundColor: "#eee",
