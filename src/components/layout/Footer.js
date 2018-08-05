6d70843ec8fd57075fb258d5a9961e11c8df9a91
pscroce <pscroce@gmail.com>
2018-08-05 00:35:22 -0400

    feat: add countdown and link to home page in footer

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -1,19 +1,50 @@
 import React from 'react';
 import Radium from 'radium';
+import { Link } from 'react-router-dom';
 
+import Paragraph from '../layout/elements/Paragraph';
 import Section from '../layout/elements/Section';
 
+// make it update https://reactjs.org/docs/state-and-lifecycle.html
+
 function Footer() {
+  let arrival = new Date('August 25, 2018 00:30:00'); // 8:30 pm Aug 24 EST in GMT (EST + 4)
+  let now = new Date();
+  let countdown = arrival - now;
+  function convertMS(ms) {
+    var d, h, m, s;
+    s = Math.floor(ms / 1000);
+    m = Math.floor(s / 60);
+    s = s % 60;
+    h = Math.floor(m / 60);
+    m = m % 60;
+    d = Math.floor(h / 24);
+    h = h % 24;
+    return { d: d, h: h, m: m, s: s };
+  };
+  countdown = convertMS(countdown);
   return (
     <Section>
       <div style={styles.footer} className="footer">
-        {/* <div style={styles.footerText}> */}
-          {/* <span style={styles.footerItem}> </span> */}
-          <span style={styles.footerItem} role="img" aria-label="Statue of Liberty Emoji">
+        <div style={styles.footerText}>
+          <span style={[styles.footerItem, styles.footerItem.sides]}>
+            <Paragraph>
+              <Link to="/">
+              <span role="img" aria-label="Statue of Liberty Emoji">
+                🏡
+              </span>
+              </Link>
+            </Paragraph>
+          </span>
+          <span style={[styles.footerItem, styles.footerItem.middle]} role="img" aria-label="Statue of Liberty Emoji">
               🗽
           </span>
-          {/* <span style={styles.footerItem}> </span> */}
-        {/* </div> */}
+          <span style={[styles.footerItem, styles.footerItem.sides]}>
+            <Paragraph>
+              {`${countdown.d} days ${countdown.h} hours and ${countdown.m} minutes`}
+            </Paragraph>
+          </span>
+        </div>
 
       </div>
     </Section>
@@ -28,18 +59,20 @@ const styles = {
     width: '100%',
     height: 50,
     '@media (max-width: 480px)': { // mobile
+      height: 200,
     },
   },
   footerText: {
     display: 'flex',
-    justifyContent: 'center',
     flex: 1,
     flexDirection: 'row',
+    justifyContent: 'space-around',
+
     // width: '90%',
     margin: 0,
     fontSize: '5rem',
     '@media (max-width: 992px)': { // tablet
-      width: '100%',
+      // width: '100%',
       // flexDirection: 'column',
       // alignItems: 'center',
       // justifyContent: 'space-around',
@@ -48,17 +81,20 @@ const styles = {
     },
   },
   footerItem: {
-    width: 250,
+    width: 350,
     textAlign: 'center',
     fontSize: 80,
-    marginBottom: -10,
     '@media (max-width: 992px)': { // tablet
-      width: 'none',
+      width: 'auto',
     },
+    sides: {
+      marginBottom: 25,
+    },
+    middle: {
+      position: 'relative',
+      top: 20,
+    }
   },
-  footerItemMiddle: {
-    padding: '0 10px',
-  }
 }
 
 export default Radium(Footer);
